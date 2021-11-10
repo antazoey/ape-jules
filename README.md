c65fba4c43732c6544b73d66d15e7c3229f3abef
unparalleled-js <yingthi@live.com>
2021-11-10 14:38:38 -0600

    fix: fixes

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # ape-jules
 
 Jules' custom ape plugin
+
+## Commands
+
+### test-networks
+
+Usage:
+
+```bash
+ape jules test-network --network <option>
+```
+
+Description:
+
+Test the current network by connecting to it and printing its name.
--- ape_jules/_cli.py
+++ ape_jules/_cli.py
@@ -4,33 +4,6 @@ import click
 
 from ape import networks
 from ape.cli import network_option, NetworkBoundCommand
-from eth_typing import ChecksumAddress, HexAddress, HexStr
-
-
-def _abi_callback(arg):
-    try:
-        file_json = json.load(arg)
-        if "abi" in file_json:
-            return file_json["abi"]
-
-        return file_json
-    except Exception:
-        raise click.BadOptionUsage("Was not given a proper ABI JSON file.")
-
-
-address_option = click.option(
-    "--address",
-    callback=lambda ctx, param, arg: ChecksumAddress(HexAddress(HexStr(arg))),
-    help="The contract address",
-    required=True,
-)
-abi_option = click.option(
-    "--abi",
-    type=click.File(),
-    callback=lambda ctx, param, arg: _abi_callback(arg),
-    help="The contract ABI",
-    required=True,
-)
 
 
 @click.group()

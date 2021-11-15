2b2dba20c55e912a978cd33366153942ddc391fa
unparalleled-js <yingthi@live.com>
2021-11-15 10:58:23 -0600

    feat: add balance command

--- ape_jules/_cli.py
+++ ape_jules/_cli.py
@@ -1,7 +1,10 @@
 import click
-
 from ape import config, networks
-from ape.cli import network_option, NetworkBoundCommand
+from ape.cli import (
+    NetworkBoundCommand,
+    account_option_that_prompts_when_not_given,
+    network_option,
+)
 
 
 @click.group(short_help=config.get_config("jules")["message"])
@@ -12,5 +15,20 @@ def cli():
 @cli.command(cls=NetworkBoundCommand)
 @network_option()
 def ping(network):
-    """Test the connection the network"""
+    """
+    Test the connection the network
+    """
+    _ = network
     click.echo(f"Currently connected to {networks.active_provider.name}")
+
+
+@cli.command(cls=NetworkBoundCommand)
+@network_option()
+@account_option_that_prompts_when_not_given()
+def balance(network, account):
+    """
+    Check the balance of an account
+    """
+    _ = network
+    amount = networks.active_provider.get_balance(account.address)
+    click.echo(amount)

d5d1b0d393fd35bc7631565947150d56661c853f
unparalleled-js <yingthi@live.com>
2022-02-01 23:17:13 -0600

    fix: accounts option update

--- ape_jules/_cli.py
+++ ape_jules/_cli.py
@@ -1,12 +1,16 @@
+import curses
 import json
+import random
 import shutil
+import time
 
 import click
 from ape import accounts, chain, config, networks
 from ape.cli import (
     NetworkBoundCommand,
-    account_option_that_prompts_when_not_given,
-    network_option, contract_option,
+    account_option,
+    contract_option,
+    network_option,
 )
 
 
@@ -29,12 +33,14 @@ def ping(network):
     ecosystem_name = provider.network.ecosystem.name
     network_name = provider.network.name
     provider_name = provider.name
-    click.echo(f"Current connected to :{ecosystem_name}:{network_name}:{provider_name}'.")
+    click.echo(
+        f"Current connected to :{ecosystem_name}:{network_name}:{provider_name}'."
+    )
 
 
 @cli.command(cls=NetworkBoundCommand)
 @network_option()
-@account_option_that_prompts_when_not_given()
+@account_option()
 def balance(network, account):
     """
     Check the balance of an account
@@ -82,7 +88,7 @@ def get_block(network, block_id):
 
 
 @cli.command(cls=NetworkBoundCommand)
-@account_option_that_prompts_when_not_given()
+@account_option()
 @network_option()
 def nonce(account, network):
     """
@@ -98,6 +104,7 @@ def test_accounts(limit):
     """
     Print all the test accounts.
     """
+
     def _yield_accounts():
         index = 0
         for acct in accounts.test_accounts:
@@ -154,3 +161,119 @@ def poll_blocks(network):
         click.echo(
             f"New block found: number={new_block.number}, timestamp={new_block.timestamp}, size={new_block.size}"
         )
+
+
+@cli.command()
+def play_snake():
+    """
+    Play a game of Snake.
+    """
+
+    # initialize screen
+    sc = curses.initscr()
+    h, w = sc.getmaxyx()
+    win = curses.newwin(h, w, 0, 0)
+
+    win.keypad(1)
+    curses.curs_set(0)
+
+    # Initial Snake and Apple position
+    snake_head = [10, 15]
+    snake_position = [[15, 10], [14, 10], [13, 10]]
+    apple_position = [20, 20]
+    score = 0
+
+    # display apple
+    win.addch(apple_position[0], apple_position[1], curses.ACS_DIAMOND)
+
+    prev_button_direction = 1
+    button_direction = 1
+    key = curses.KEY_RIGHT
+
+    def collision_with_apple(score):
+        apple_position = [random.randint(1, h - 2), random.randint(1, w - 2)]
+        score += 1
+        return apple_position, score
+
+    def collision_with_boundaries(snake_head):
+        if (
+            snake_head[0] >= h - 1
+            or snake_head[0] <= 0
+            or snake_head[1] >= w - 1
+            or snake_head[1] <= 0
+        ):
+            return 1
+        else:
+            return 0
+
+    def collision_with_self(snake_position):
+        snake_head = snake_position[0]
+        if snake_head in snake_position[1:]:
+            return 1
+        else:
+            return 0
+
+    a = []
+    while True:
+        win.border(0)
+        win.timeout(100)
+
+        next_key = win.getch()
+
+        if next_key == -1:
+            key = key
+        else:
+            key = next_key
+
+        # 0-Left, 1-Right, 3-Up, 2-Down
+        if key == curses.KEY_LEFT and prev_button_direction != 1:
+            button_direction = 0
+        elif key == curses.KEY_RIGHT and prev_button_direction != 0:
+            button_direction = 1
+        elif key == curses.KEY_UP and prev_button_direction != 2:
+            button_direction = 3
+        elif key == curses.KEY_DOWN and prev_button_direction != 3:
+            button_direction = 2
+        else:
+            pass
+
+        prev_button_direction = button_direction
+
+        # Change the head position based on the button direction
+        if button_direction == 1:
+            snake_head[1] += 1
+        elif button_direction == 0:
+            snake_head[1] -= 1
+        elif button_direction == 2:
+            snake_head[0] += 1
+        elif button_direction == 3:
+            snake_head[0] -= 1
+
+        # Increase Snake length on eating apple
+        if snake_head == apple_position:
+            apple_position, score = collision_with_apple(score)
+            snake_position.insert(0, list(snake_head))
+            a.append(apple_position)
+            win.addch(apple_position[0], apple_position[1], curses.ACS_DIAMOND)
+
+        else:
+            snake_position.insert(0, list(snake_head))
+            last = snake_position.pop()
+            win.addch(last[0], last[1], " ")
+
+        # display snake
+        win.addch(snake_position[0][0], snake_position[0][1], "#")
+
+        # On collision kill the snake
+        if (
+            collision_with_boundaries(snake_head) == 1
+            or collision_with_self(snake_position) == 1
+        ):
+            break
+
+    sc.addstr(10, 30, "Your Score is:  " + str(score))
+    sc.refresh()
+    time.sleep(2)
+    curses.endwin()
+    click.echo(a)
+    click.echo(w, h)
